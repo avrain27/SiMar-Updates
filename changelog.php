97120140a80643cde897097d0faa69f4c145f76b
mimingo <wernertn@umich.edu>
2017-02-03 01:06:40 -0500

    Version 0.0.8.3
    Changes to async / defer that hopefully works with Pagespeeds

--- changelog.php
+++ changelog.php
@@ -4,6 +4,8 @@
 	<title>SiMar Template: Changelog</title>
 </head>
 <body>
+|==== Version 0.0.8.3 =====|
+Changes that hopefully work...
 |==== Version 0.0.8.2 =====|
 Added defer to jquery.js
 |==== Version 0.0.8.1 =====|
