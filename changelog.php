896847e34d56fd0e327b6c69444ca879bc3854e5
mimingo <wernertn@umich.edu>
2017-02-03 02:10:04 -0500

    Version 0.0.8.5

--- changelog.php
+++ changelog.php
@@ -4,6 +4,8 @@
 	<title>SiMar Template: Changelog</title>
 </head>
 <body>
+|==== Version 0.0.8.5 =====|
+css include tweaks
 |==== Version 0.0.8.4 =====|
 Reversed async/defer on scripts
 |==== Version 0.0.8.3 =====|
